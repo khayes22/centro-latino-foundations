563b8355ec82eca520497858f7a656c1b8826e25
khayes22 <khayes5482020@gmail.com>
2022-07-13 21:24:34 -0500

    first-javascript 7/13

---
+++ Interface Online Courses/first-javascript/first-javascript.js
@@ -0,0 +1,173 @@
+// var student = "Jill";
+// console.log(student);
+// console.log("The name of the student is " + student);
+// console.log("student");
+// console.log(2);
+// console.log('2');
+// console.log("good" + "morning");
+// console.log("good" + " " + "morning");
+// console.log("good " + "morning");
+// var firstName = "Evan";
+// var lastName = "Henderson";
+// console.log(firstName +  " " + lastName);
+// console.log(2 + 5);
+// console.log('2' + '5');
+// console.log(2 + '5');
+// // console.log('2' + 5);
+// var cities = ["Oakland", "Chicago", "Pittsburgh"];
+// console.log(cities[1]);
+// console.log(cities[2]);
+// console.log(cities[0]);
+// console.log(cities[3]);
+// var points = [25, 36, 42, 81, 50];
+// console.log(points[4]);
+// var entries = ["Steve", 25, "Bruce", true];
+// console.log(entries[1]);
+// console.log(entries[0]);
+// console.log(entries[3]);
+// console.log(cities);
+// console.log(cities.length);
+// cities.push("Omaha");
+// console.log(cities.length);
+// console.log(cities);
+// cities.push("Ames", "Beatrice", "Springfield");
+// console.log(cities.length);
+// console.log(cities);
+// cities.pop();
+// console.log(cities.length);
+// console.log(cities);
+// cities.splice(2,2);
+// console.log(cities);
+// cities.splice(1);
+// console.log(cities);
+var company = {
+    companyName: "AIM Institute",
+    federalTaxId: 84827690,
+    sections: [
+        {
+            sectionName: "AIM Code School",
+            instructors: [
+                {
+                    firstName: "Vanessa",
+                    lastName: "Kasun",
+                },
+                {
+                    firstName: "David",
+                    lastName: "Tarvin",
+                }
+            ],
+            numOfStudents: 26,
+            sectionHead: "Erin Lassiter"
+        },
+        {
+            sectionName: "Careerlink",
+            sectionHead: "Nate Decker",
+        }
+    ]
+}
+// console.log(company);
+// console.log("AIM's federal Tax Id is" + company.federalTaxId);
+// console.log("Betty said, 'I made a great pie!' and asked us to taste it.");
+// console.log(company.sections[0].sectionName);
+// console.log(company.sections[0].instructors[1].firstName);
+// var count = 1;
+// console.log('Count is now' + count);
+// count = count + 5;
+// console.log('count = count + 5 Count is now' + count);
+// count += 3;
+// console.log('count += 3 Count is now' + count);
+// count++;
+// console.log('count++ Count is now' + count);
+// var x = 18;
+// var y = 9
+// console.log( x + ' divided by' + y + '=' + (x/y));
+// console.log('The remainder of' + x + 'divided by' + y + 'is' + (x % y));
+// y = 12;
+// console.log( x + ' divided by' + y + '=' + (x/y));
+// console.log('The remainder of' + x + 'divided by' + y + 'is' + (x % y));
+// x = 24;
+// console.log( x + ' divided by' + y + '=' + (x/y));
+// console.log('The remainder of' + x + 'divided by' + y + 'is' + (x % y));
+var x = 9;
+// console.log('Is x == 8 true or false?' + (x == 8));
+// console.log('is x == 9 true or false' + (x == 9));
+// console.log('is x == "9" true or false' + (x == "9"));
+// console.log('is x === 9 true or false' + (x === 9));
+// console.log('is x === "9" true or false' + (x === "9"));
+// console.log('is x != 8 true or false' + (x != 8));
+// console.log('is x != "8" true or false' + (x != "8"));
+// console.log('is x != 9 true or false' + (x != 9));
+// console.log('is != "9" true or false' + (x != "9"));
+// console.log('is x !== "9" true or false' + (x !== "9"));
+// console.log('is x !== to 9 true or false' + (x !== 9));
+// console.log('is x < 10 true or false' + (x < 10));
+// console.log('is x < 9 true or false' + (x < 9));
+// console.log('is x > 8 true or false' + (x > 8));
+// console.log('is x > 9 true or false' + (x > 9));
+// console.log('is x <= 9 true or false' + (x <= 9));
+// console.log('is x >= 9 true or false' + (x >= 9));
+// var goodDeal;
+// var toyCar = 5.00;
+// goodDeal = (toyCar < 1.00 ) ? "Let's Buy It" : "Let's skip it";
+// console.log(goodDeal);
+// var x = 15;
+// var y = 21;
+// console.log('x > 10 && y < 25 ' + (x > 10 && y < 25));
+// console.log('x > 20 && y < 25' + (x > 20 && y < 25));
+// console.log( 'x > 10 && y < 20' + (x > 10 && y < 20));
+// console.log('x > 20 && y < 20' + (x > 20 && y < 20));
+// console.log('======================');
+// console.log('x > 10 || y < 25' + (x > 10 || y < 25));
+// console.log('x > 20 || y < 25' + (x > 20 || y < 25));
+// console.log('x > 10 || y < 20' + (x > 10 || y < 20));
+// console.log('x > 20 || y < 20' + (x > 20 || y < 20));
+// console.log('======================');
+// console.log('!(x > 20' + !(x > 20));
+// console.log('!(x < 20' + !(x< 20));
+// console.log('x > 10 && !(y < 25 '+ (x > 10 && !(y < 25)));
+// console.log('!(x > 20 && y < 20' + !(x > 20 && y < 20));
+
+// var zombies = 28;
+// if (zombies > 30) {
+//     console.log('AAAARGH Too Many Zombies');
+// } 
+
+// zombies=10;
+// if (zombies > 20) {
+//     console.log('AAAARGH Too Many Zombies');
+// } else {
+//     console.log('Glad it is only a few zombies');
+// }
+
+// if (zombies = 0) {
+//     console.log('No zombies here');
+// } else if (zombies <= 10) {
+//     console.log('Glad it is only a few zombies')
+// } else if (zombies <= 20) {
+//     console.log('Yikes!')
+// } else {
+//     console.log('Zombies!!');
+// }
+
+var grade = 50;
+
+if (grade >= 90) {
+    console.log('You got an A');
+} else if (grade >=80 && grade <= 89) {
+    console.log('You got a B');
+} else if (grade >= 70) {
+    console.log('You got a C');
+} else if (grade >= 60) {
+    console.log('You got a D');
+} else {
+    console.log('Failure!')
+}
+
+var goldCoins = 29;
+if (goldCoins > 5) {
+    console.log('A bit of pocket change');
+} else if (goldCoins > 10) {
+    console.log('Great!');
+} else if (goldCoins > 20) {
+    console.log('I am rich');
+}
